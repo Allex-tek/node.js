dc67190fb333fa1a30e1afa79281688421b9002e
Allex-Lima <alexlmangabeira@gmail.com>
2025-07-01 14:07:10 -0300

    criando rotas com parâmetros(regex)

---
+++ 01-fundamentos-nodejs/src/utils/build-route-path.js
@@ -0,0 +1,8 @@
+// path: '/users/1',
+
+export function buildRoutePath(path) {
+    const routeParametersRegex = /:([a-zA-Z]+)/g
+
+    console.log(Array.from(path.matchAll(routeParametersRegex)));
+    
+}
